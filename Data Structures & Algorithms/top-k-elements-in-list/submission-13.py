80ca94504100c22469ff4d16f619c11921af3945
Thomas Motais <slimey.ninja2024@gmail.com>
2026-09-07 19:28:08 -0400

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        f = defaultdict(int)
+        # step 1: get a frequency dict of elements
+        for number in sorted(nums):
+            if number not in f:
+                f[number] = 1
+            else:
+                f[number] += 1
+        
+        # step 2: retrieve k highest frequency
+        final = []
+        for i in range(k):
+            high = max(f.values())
+            for num in f:
+                if f[num] == high:
+                    print('num', num, 'high', high)
+                    final.append(num)
+                    f[num] = -1
+                    break
+        print(final)
+
+        return final
